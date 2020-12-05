83af5a74571a6135f7952b2952374f574580ad80
Xavier Campos Diaz <xavicampos99@gmail.com>
2020-12-05 20:19:14 +0100

    Analyze querys, and implement Q1

--- entrega.py
+++ entrega.py
@@ -20,44 +20,125 @@ def printOptions():
 def initializeDB():
     print("Initializing DB!")
     posts = []
+    collection = db["LineItem"]
     for i in range(200):
         order_key = np.random.randint(1, 20)
+        order_date = datetime(np.random.randint(2000, 2020), np.random.randint(1, 12), np.random.randint(1, 27))
+        order_shippriority = random.choice(string.ascii_uppercase)*15
+        customer_mkt_seg = random.choice(string.ascii_uppercase)*10
+        customer_nation_key = np.random.randint(1, 25)
+        order = {"_id": order_key, "order_date": order_date, "shippriority": order_shippriority, "c_mktsegment": customer_mkt_seg, "c_nationkey": customer_nation_key}
         quantity = float(np.random.randint(1, 5))
         extendedprice = float(np.random.randint(5, 1500) + np.random.randint(0, 99)/100)
         discount = float(np.random.randint(0, 65)/100)
         tax = float(random.choice([4,10,21])/100)
-        shipdate = datetime(np.random.randint(2000, 2020), np.random.randint(1, 12), np.random.randint(1, 30))
-        returnflag = random.choice(string.ascii_letters)
-        linestatus = random.choice(string.ascii_letters)
-        posts.append({"_id":i, "order_key": order_key, "quantity": quantity, "extendedprice": extendedprice, 
+        shipdate = datetime(np.random.randint(2000, 2020), np.random.randint(1, 12), np.random.randint(1, 27))
+        returnflag = random.choice(string.ascii_uppercase)
+        linestatus = random.choice(string.ascii_uppercase)
+        posts.append({"_id":i, "order": order, "quantity": quantity, "extendedprice": extendedprice, 
                     "discount": discount, "tax": tax, "shipdate": shipdate, "returnflag": returnflag, "linestatus": linestatus})
 
     collection.insert_many(posts)
 
+    collection = db["PartSupp"]
+    posts = []
+    for i in range(200):
+        ps_supplycost = float(np.random.randint(5, 1500) + np.random.randint(0, 99)/100)
+        p_key = np.random.randint(1, 20)
+        p_mfgr = random.choice(string.ascii_uppercase)*25
+        p_type = random.choice(string.ascii_uppercase)*25
+        p_size = np.random.randint(1, 50)
+        part = {"_id": p_key, "mfgr": p_mfgr, "type": p_type, "size": p_size}
+
+        s_suppkey = np.random.randint(1, 20)
+        s_name = random.choice(string.ascii_uppercase)*25
+        s_address = random.choice(string.ascii_uppercase)*40
+        s_phone = random.choice(string.ascii_uppercase)*15
+        s_acctbal = float(np.random.randint(5, 1500) + np.random.randint(0, 99)/100)
+        s_comment = random.choice(string.ascii_uppercase)*101
+        n_name = random.choice(string.ascii_uppercase)*25
+        r_name = random.choice(string.ascii_uppercase)*25
+        supplier = {"_id": s_suppkey, "name": s_name, "address": s_address, "phone": s_phone, "acctbal": s_acctbal, "comment": s_comment, "nation_name": n_name, "region_name": r_name}
+        PartSupp = {"_id": i, "part": part, "supplier": supplier, "supplycost": ps_supplycost}
+        posts.append(PartSupp)
+        
+    collection.insert_many(posts)
+
 def execute_q1(date):
     results = collection.aggregate( [
-        {"$match": {
-            "shipdate": {"$gt": date }
-        }},
-        {"$project": {
-            "_id": 0,
-            "returnflag": 1,
-            "linestatus": 1,
-            "sum_qty": {"$sum": "$quantity"},
-            "sum_base_price": {"$sum": "$extendedprice"},
-            "sum_disc_price": { "$multiply": [ {"$sum": "$extendedprice"}, {"$subtract": [1, "$discount"] }]},
-            "sum_charge": { "$multiply": [ { "$multiply": [ {"$sum": "$extendedprice"}, {"$subtract": [1, "$discount"] }]}, {"$sum": [1, "$tax"]}]},
-            "avg_qty": { "$avg": "$quantity"},
-            "avg_price": { "$avg": "$extendedprice"},
-            "avg_disc": { "$avg": "$discount"}
-        }},
-        {"$group": {
-            "_id" : { "returnflag": "$returnflag", "linestatus": "$linestatus" },   
-        }},
-        {"$sort": {
-            "_id.returnflag":1, "_id.linestatus":1
-        }}
-    ])
+    {
+        '$match': {
+            'shipdate': {
+                '$lte': date
+            }
+        }
+    }, {
+        '$addFields': {
+            'perc_disc': {
+                '$subtract': [
+                    1, '$discount'
+                ]
+            }, 
+            'perc_tax': {
+                '$add': [
+                    1, '$tax'
+                ]
+            }
+        }
+    }, {
+        '$addFields': {
+            'disc_price': {
+                '$multiply': [
+                    '$extendedprice', '$perc_disc'
+                ]
+            }
+        }
+    }, {
+        '$addFields': {
+            'charge': {
+                '$multiply': [
+                    '$disc_price', '$perc_tax'
+                ]
+            }
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'returnflag': '$returnflag', 
+                'linestatus': '$linestatus'
+            }, 
+            'sum_qty': {
+                '$sum': '$quantity'
+            }, 
+            'sum_base_price': {
+                '$sum': '$extendedprice'
+            }, 
+            'sum_disc_price': {
+                '$sum': '$disc_price'
+            }, 
+            'sum_charge': {
+                '$sum': '$charge'
+            }, 
+            'avg_qty': {
+                '$avg': '$quantity'
+            }, 
+            'avg_price': {
+                '$avg': '$extendedprice'
+            }, 
+            'avg_disc': {
+                '$avg': '$discount'
+            }, 
+            'count_order': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$sort': {
+            '_id.returnflag': 1, 
+            '_id.linestatus': 1
+        }
+    }
+])
     for x in results:
         print(x)
 
